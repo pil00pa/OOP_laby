66b1a5f5b6a8182e07268549cf091afe9a155f5f
pil00pa <valeriy.mikhaylov2003@gmail.com>
2023-01-10 05:28:04 +0200

    This is end 2

--- OOP.cpp
+++ OOP.cpp
@@ -13,14 +13,35 @@ int Data::objectCount = 0;
 
 int main()
 {
+	Product p("Coca Cola light", 144, 15.55, 0.5, forLower);
+	list<Product> pro; pro.push_back(p);
+
+	User u("Mishania", 332, pro);
+	User u2("Data.csv");
+	cout << u[0];
 	System s;
+
+
+	s.addUser(u);
+
+
 	for (int i = 0; i < 100; i++)
 	{
-		Product p("govno", i + 100, 123, 22, forLower);
-		s.addProduct(p);
+		s.addUser(User("penis", i, pro));
 	}
 
-	cout << s.getPopularProductForMiddle().size();
 
-	
+	cout << (s._UserBase.find(55))->second->getPhone();
+	cout << s.getUserByPhone(33).getPhone();
+
+	cout << "\nData demo\n";
+	Data data;
+	cout << data;
+	Data data2(28, 2, 2004);
+	cout << data2;
+	++data2;
+	cout << data2;
+	cout << "Static member demo\n";
+	cout << data.objectCount << "\n";
+	cout << data.objectCount << "\n";
 }
